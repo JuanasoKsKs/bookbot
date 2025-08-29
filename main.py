9cd188bed7314cf1f02380cdfea36dc3ec24bc98
JuanasoKsKs <jesse.bmz16@gmail.com>
2025-08-28 19:23:53 -0600

    G:Add 'Sys.argv' and finalize project

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from stats import words_in_book, get_char, dict_to_sorted_list
+import sys
 
 def get_book_text(book_path):
     with open(book_path) as f:
@@ -6,17 +7,21 @@ def get_book_text(book_path):
     return file_contents
 
 def main():
-    path = "books/frankenstein.txt"
-    text = get_book_text(path)
-    print("===========BOOKBOT============")
-    print(f"Analyzing book found at {path}...")
-    print("---------Word Count-----------")
-    print(f"Found {words_in_book(text)} total words")
-    print("------ Character Count -------")
-    first_dic = get_char(text)
-    last_dictionary = dict_to_sorted_list(first_dic)
-    for each in last_dictionary:
-        if each["char"].isalpha():
-            print(f"{each["char"]}: {each["num"]}")
+    if len(sys.argv) == 2:
+        path = sys.argv[1]
+        text = get_book_text(path)
+        print("===========BOOKBOT============")
+        print(f"Analyzing book found at {path}...")
+        print("---------Word Count-----------")
+        print(f"Found {words_in_book(text)} total words")
+        print("------ Character Count -------")
+        first_dic = get_char(text)
+        last_dictionary = dict_to_sorted_list(first_dic)
+        for each in last_dictionary:
+            if each["char"].isalpha():
+                print(f"{each["char"]}: {each["num"]}")
+    else:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
      
 main()
